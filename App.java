2248249f4587538d9270f7101d5c81ab697d4d41
Tomas Rehak <trehak1@gmail.com>
2016-03-23 13:13:52 +0100

    Update App.java

--- App.java
+++ App.java
@@ -7,8 +7,8 @@ import java.nio.charset.*;
  *
  */
 public class App {
-    public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
         // staci "Hello world", neni treba ten StringBuffer 
         // +1 za StandardCharsets
 	HashCode hashCode = Hashing.sha1().hashString("Hello World", StandardCharsets.UTF_8);
